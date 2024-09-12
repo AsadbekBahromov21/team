e865aef182111c2ffd636bae00676fb38b85a11d
AsadbekBahromov21 <bahromov1621a@gmail.com>
2024-09-12 11:53:47 +0500

    companentslar qoshib qoyildi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React, { lazy, Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+const App = lazy(() => import("./App"));
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Suspense
+      fallback={
+        <div className="loader flex flex-col gap-4 m-auto mt-[300px]">
+          loading...
+        </div>
+      }
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
+  </React.StrictMode>
+);
