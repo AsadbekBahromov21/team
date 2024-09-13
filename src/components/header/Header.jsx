b5f68c5a9917e0234fd85417894c5ab6a8f192b4
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-09-13 09:15:20 +0500

    header qismi skeleti tayyor

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Navbar from "../navbar/Navbar";
 
 const Header = () => {
-  return <div>Header</div>;
-};
+  return 
+  <div className="w-full flex flex-col items-center container mx-auto">
+      <div className="w-full h-[38px] bg-black">
+          <span></span>
+      </div>
+      <Navbar/>
+  </div>
+}
 
 export default Header;
