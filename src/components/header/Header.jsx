25737cfdb6758b5ac98b2c5a1dd5f62b5cba0113
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-09-13 09:27:56 +0500

    header qilindi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import { FiX } from "react-icons/fi";
 
 const Header = () => {
-  return 
-  <div className="w-full flex flex-col items-center container mx-auto">
-      <div className="w-full h-[38px] bg-black">
-          <span></span>
-      </div>
-      <Navbar/>
-  </div>
-}
+  return    <div className="w-full flex flex-col items-center">
+              <div className="w-full h-[38px] bg-black flex justify-center items-center">
+                <div className="container mx-auto flex items-center justify-center">
+                  <span className="text-white text-[14px] font-normal text-center flex-1">
+                    Sign up and get 20% off to your first order. Sign Up Now
+                  </span>
+                  <FiX className="text-white"/> 
+                </div>
+              </div>
+              <Navbar/>
+            </div>
+};
 
 export default Header;
