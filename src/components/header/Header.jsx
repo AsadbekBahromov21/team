b5a0737e5a6ad440c73a260af2db25770854347e
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-09-13 10:12:41 +0500

    headerni boldim mediasiz

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "../navbar/Navbar";
 import { FiX } from "react-icons/fi";
 
 const Header = () => {
-  return    <div className="w-full flex flex-col items-center">
+  return    <div className="w-full flex flex-col items-center gap-6 flex-1 font-satoshi">
               <div className="w-full h-[38px] bg-black flex justify-center items-center">
                 <div className="container mx-auto flex items-center justify-center">
                   <span className="text-white text-[14px] font-normal text-center flex-1">
@@ -12,7 +12,7 @@ const Header = () => {
                   <FiX className="text-white"/> 
                 </div>
               </div>
-              <Navbar/>
+              <Navbar items={["Shop", "On Sale", "New Arrivals", "Brands"]}/>
             </div>
 };
 
